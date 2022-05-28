2f7ec6967dd187ccd48a57561615a6c78e97964e
Pavel <wakeful@mail.ru>
2022-05-28 19:20:10 +0300

    TextComit 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _headerLabel;
+    [SerializeField] private TextMeshProUGUI _storyLabel;
+    [SerializeField] private TextMeshProUGUI _choicesLabel;
+}
